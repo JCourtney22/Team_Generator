a5f0b2fd6faf032125233bf6dc05e79a2c392dcf
JCourtney22 <115502589+JCourtney22@users.noreply.github.com>
2023-01-29 14:18:28 +0000

    Added constructors to the role js files.

--- assets/lib/Engineer.js
+++ assets/lib/Engineer.js
@@ -1,2 +1,9 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 const Engineer = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, github) {
+        super(name, id, email);
+        this.github = github;
+    }
+}
--- assets/lib/Intern.js
+++ assets/lib/Intern.js
@@ -1,2 +1,9 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
-const Inter = require("./Employee");
+const Intern = require("./Employee");
+
+class Intern extends Employee {
+    constructor(name, school) {
+        super(name, id, email);
+        this.school = school;
+    }
+}
